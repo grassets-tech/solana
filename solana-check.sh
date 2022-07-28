a45539b8efdab95fc663c14631228406d9e4bcd8
GRASSETS TECH LIMITED <82155440+grassets-tech@users.noreply.github.com>
2022-07-28 01:35:26 +0100

    Update and rename solana-check to solana-check.sh

--- solana-check.sh
+++ solana-check.sh
@@ -3,7 +3,6 @@ echo "Balance: $(solana balance ~/validator-keypair.json)"   ; \
 echo "Balance account: $(solana balance ~/vote-account-keypair.json)"   ; \
 solana epoch-info   ; \
 solana block-production   | grep -e " Identity\|$(solana-keygen pubkey ~/validator-keypair.json)" ; \
-solana validator-info get 2ZvPKQYuuMhQEYMtcVtJHjDwTqdKiCWF2TBHDrFZMuEa   ; \
 solana validators   | grep -e "Identity\|9FXD1NXrK6xFU8i4gLAgjj2iMEWTqJhSuQN8tQuDfm2e" ; \
 echo "running: "$(curl -s -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","id":1, "method":"getVersion"}' http://localhost:8899 | jq -r '.result."solana-core"' ); \
 echo "installed: "$(solana -V)
